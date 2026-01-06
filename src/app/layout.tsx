40edbaaff54ac9f4758b817342160c899b7cf9b1
Boluwatife180872 <bolu.onukwu@gmail.com>
2026-01-06 15:06:37 +0100

    Update layout behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,20 +16,6 @@ export default function RootLayout({
 }>) {""
   return (
     <html lang="en">
-      <head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17840759178"
-        ></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-17840759178');`}
-        </script>
-      </head>
       <body>
         {children}
         <ToastContainer style={{ zIndex: 9999999999 }} />
