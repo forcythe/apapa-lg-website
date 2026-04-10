4dfafd993fbc9aea3a2fd60ae0eb57f7cf853eef
Abdulraqeeb taiwo <taiworoqeeb@gmail.com>
2026-04-10 15:55:00 +0100

    -a

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  // output: 'export',
   images: {
     remotePatterns: [
       {
